a0217de7bd98f2756bc1669d1e353c31bb272bbe
Prokhorov Alexander <streamgroupru@gmail.com>
2017-10-30 08:58:03 +0300

    Add some standsrt code at files

--- contacts.php
+++ contacts.php
@@ -1,1 +1,21 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset = "utf-8" />
+    <title>Контакты</title>
+</head>
+<body>
+<ul id="nav">
+    <li><a href="index.php">Главная</a></li>
+    <li><a href="service.php">Услуги</a></li>
+    <li><a href="contacts.php">Контакты</a></li>
+    <li><a href="about.php">О нас</a></li>
+</ul>
+
+<h2>Контакты</h2>
+
+<p>Связвться с нами можно по телефону: 8-975-958-65-52 <br>
+    или по электронной почте showstatusyoursyte@showstats.com</p>
+
+</body>
+</html>
